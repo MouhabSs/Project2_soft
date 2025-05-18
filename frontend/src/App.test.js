a43f4c437d87d074455aaee030aa6471caae8e5b
Mouhab Abdelaziz <Mo.Samer@nu.edu.eg>
2025-05-19 01:55:55 +0300

    test: add test for home page heading in App.test.js
    Add a new test case to verify the presence of the home page heading in the App component. This ensures that the heading is correctly rendered and improves test coverage.

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,11 +2,17 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 // Mock the useAuth hook
-jest.mock('./AuthProvider', () => ({
+jest.mock('./auth/AuthProvider', () => ({
   useAuth: () => ({
     isAuthenticated: true, // or false, depending on your test scenario
   }),
 }));
+
+test('renders home page heading', () => {
+  render(<App />);
+  const headingElement = screen.getByText(/Welcome to the Nutrition Clinic App/i);
+  expect(headingElement).toBeInTheDocument();
+});
 
 test('renders learn react link', () => {
   render(<App />);
